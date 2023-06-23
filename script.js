9847887560488535390bb6a31db80a1aca85a84b
Brandon-Lyons98 <108233523+Brandon-Lyons98@users.noreply.github.com>
2023-06-23 13:10:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -85,34 +85,4 @@ colorWheel.addEventListener('input', () => {
     }
 });
 
-/*
-function makeSquareBoxes() {
-    const boxes = document.createElement('div');
-    boxes.classList.add('box');
-    boxes.addEventListener('mouseover', () => {
-        let a = randomizeBoxColor();
-        let b = randomizeBoxColor();
-        let c = randomizeBoxColor();
-        
-        boxes.style.backgroundColor = 'black';
-        //boxes.style.backgroundColor = "rgb("+ a +","+ b +","+ c +")";
-    });
-
-    clear.addEventListener('click', () => {
-        boxes.style.backgroundColor = 'white';
-    });
-    
-    container.appendChild(boxes);
-}
-
-for (let i = 0; i <= 15; i++) {
-    for (let j = 0; j <= 15; j++) {
-        makeSquareBoxes();
-    }
-}
-
-
-*/
-
-
 createGrid();
